462aa29424a8ad2fab33d016c5a0ba24813c102c
Andrea Minato <andrea.minatohotmail.it>
2017-01-31 10:33:21 +0100

    sizeClasses

---
+++ Array/Array/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Array
+//
+//  Created by AndreaITS on 23/01/17.
+//  Copyright © 2017 AndreaITS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SizeClasses/SizeClasses/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SizeClasses
+//
+//  Created by AndreaITS on 31/01/17.
+//  Copyright © 2017 AndreaITS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
